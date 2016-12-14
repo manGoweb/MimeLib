6f83846af716a9a6f0e7e9217ed97184f6cc22f5
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2016-12-14 20:58:46 +0000

    Initial Commit

---
+++ MimeLibGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MimeLibGenerator
+//
+//  Created by Ondrej Rafaj on 14/12/2016.
+//  Copyright © 2016 manGoweb UK Ltd. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
